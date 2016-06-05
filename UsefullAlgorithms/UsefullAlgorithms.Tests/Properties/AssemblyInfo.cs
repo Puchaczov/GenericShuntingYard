6ab7eb234d28bd00d951eda2cd4a43426545f565
Jakub Puchała <puchala.czwa2@hotmail.com>
2016-06-05 22:34:24 +0200

    Changed author

--- UsefullAlgorithms/UsefullAlgorithms.Tests/Properties/AssemblyInfo.cs
+++ UsefullAlgorithms/UsefullAlgorithms.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UsefullAlgorithms.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Jakub Puchała ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
